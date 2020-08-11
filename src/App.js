db1606034aa3909b75df6288b5e4e41fe07e78f5
Wooddell <dawoodde@3c22fb33c306.ant.amazon.com>
2020-08-10 21:54:25 -0400

    Version 2 Edits

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Octank's Facial Rekognition Program Brought you by AWS
         </p>
         <a
           className="App-link"
